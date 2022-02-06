946bf365ccd838ae3ba90684c64c37e89c1b0081
Pedro <pedro.lucca@oliveiratrust.com.br>
2022-02-06 16:43:57 -0300

    test: api route should list all post

--- src/database/factories/RepostFactory.php
+++ src/database/factories/RepostFactory.php
@@ -2,10 +2,14 @@
 
 namespace Database\Factories;
 
+use App\Domain\Post\model\Post;
+use App\Domain\Post\model\Repost;
+use App\Domain\User\models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class RepostFactory extends Factory
 {
+    protected $model = Repost::class;
     /**
      * Define the model's default state.
      *
@@ -14,7 +18,10 @@ class RepostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            "id" => $this->faker->uuid,
+            "user_id" => User::all()->random()->id,
+            "post_id" => Post::all()->random()->id,
+            "content" => $this->faker->realText
         ];
     }
 }
--- src/tests/Feature/Post/PostApiTest.php
+++ src/tests/Feature/Post/PostApiTest.php
@@ -3,22 +3,26 @@
 namespace Tests\Feature\Post;
 
 use App\Domain\Post\model\Post;
+use App\Domain\Post\model\Repost;
 use App\Domain\User\models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 use function dd;
 
-class PostTest extends TestCase
+class PostApiTest extends TestCase
 {
     use RefreshDatabase;
-    public function test_should_list_all_user_posts()
+
+    public function test_should_list_all_posts()
     {
         $user = User::factory()->create();
         Auth::setUser($user);
-        $post = Post::factory()->count(3)->create();
+        $post = Post::factory()->count(1)->create();
+        $repost = Repost::factory()->count(1)->create();
+        $totalPosts = $post->count() + $repost->count();
         $response = $this->get('api/post');
         $response->assertStatus(200);
-        $this->assertEquals($post->toArray(), $response->json());
+        $this->assertEquals($totalPosts, count($response->json()));
     }
 }
