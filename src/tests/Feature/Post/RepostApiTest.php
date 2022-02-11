e339cfb9d88f44ffc3b35e733348347e25d37931
Pedro <pedro.lucca@oliveiratrust.com.br>
2022-02-11 12:07:18 -0300

    refactor: test path

--- src/tests/Feature/Post/RepostApiTest.php
+++ src/tests/Feature/Post/RepostApiTest.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace Tests\Unit\Post;
+namespace Tests\Feature\Post;
 
 use App\Domain\Post\actions\IndentifyUserMentionAction;
 use App\Domain\Post\model\Post;
+use App\Domain\Post\model\QuotePost;
+use App\Domain\Post\model\Repost;
 use App\Domain\User\models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
