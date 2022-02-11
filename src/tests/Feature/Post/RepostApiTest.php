a4fcd7aa4ab5ae712b1755d26de329cd8d70e370
Pedro <pedro.lucca@oliveiratrust.com.br>
2022-02-11 12:08:09 -0300

    test: shouldn`t create repost when have more than five posts in a day

--- src/tests/Feature/Post/RepostApiTest.php
+++ src/tests/Feature/Post/RepostApiTest.php
@@ -94,4 +94,25 @@ class RepostApiTest extends TestCase
                 "post_id" => ["The selected post id is invalid."]
             ]);
     }
+
+    public function test_shouldnt_create_repost_when_have_more_than_five_posts_in_a_day(){
+        $users = User::factory()->count(2)
+            ->has( Post::factory()->count(2) )
+            ->has( Repost::factory()->count(2) )
+            ->has( QuotePost::factory()->count(1) )
+            ->create();
+        Auth::setUser($users->last());
+        $post = Post::factory()->create([
+            "user_id" => $users->first()->id
+        ]);
+        $mock_data = [
+            "post_id" => $post->id
+        ];
+        $response = $this->post('api/repost',$mock_data);
+        $response
+            ->assertStatus(422)
+            ->assertJson([
+                "message" => "Whoops, did you exceed the total posts. You can post only 5 posts in a day."
+            ]);
+    }
 }
