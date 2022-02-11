314f7734ab4785a1cb118395fa617892667d2d78
Pedro <pedro.lucca@oliveiratrust.com.br>
2022-02-11 01:21:22 -0300

    test: should identify user when mentioned in post content

---
+++ src/tests/Unit/Post/QuotePostTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit\Post;
+
+use App\Domain\Post\actions\IndentifyUserMentionAction;
+use App\Domain\User\models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class QuotePostTest extends TestCase
+{
+    use WithFaker;
+
+    public function test_should_identify_user_when_mentioned_in_post_content()
+    {
+        $content = $this->faker->text(10);
+        $content = $content . " {User}any_user{/User} any text {User}another_user{/User}";
+        $matches = (new IndentifyUserMentionAction())->execute($content);
+        $this->assertEquals([
+            "any_user",
+            "another_user"
+        ],$matches);
+    }
+}
