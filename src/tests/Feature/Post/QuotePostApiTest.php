ebdf9d3e8156ff4eef54dffadf31ee2abed0ed4d
Pedro <pedro.lucca@oliveiratrust.com.br>
2022-02-11 12:20:32 -0300

    test: should not quote post content greater than 777 characters

--- src/tests/Feature/Post/QuotePostApiTest.php
+++ src/tests/Feature/Post/QuotePostApiTest.php
@@ -32,4 +32,24 @@ class QuotePostApiTest extends TestCase
                 "content" => ["The content field is required."]
             ]);
     }
+
+    public function test_should_not_quote_post_content_greater_than_777_characters(){
+        $users = User::factory()
+            ->count(2)
+            ->create();
+        $post = Post::factory()->create([
+            "user_id" => $users->first()->id
+        ]);
+        Auth::setUser($users->last());
+        $mock_data = [
+            "content" => $this->faker->realText(999),
+            "post_id" => $post->id
+        ];
+        $response = $this->post('api/quotepost',$mock_data);
+        $response
+            ->assertStatus(422)
+            ->assertJson([
+                "content" => ["The content must not be greater than 777 characters."]
+            ]);
+    }
 }
