a26aa4a1d0133bded0ce6b9ea7738a11c5bf6f38
Pedro <pedro.lucca@oliveiratrust.com.br>
2022-02-11 19:00:23 -0300

    test: should create reply post when user was mentioned in post content

--- src/tests/Feature/Post/ReplyPostApiTest.php
+++ src/tests/Feature/Post/ReplyPostApiTest.php
@@ -15,4 +15,21 @@ use Tests\TestCase;
 class ReplyPostApiTest extends TestCase
 {
     use RefreshDatabase,WithFaker;
+
+    public function test_should_create_reply_post_when_user_was_mentioned_in_post_content(){
+        $mention_user = User::factory()->create(["username" => 'mention_user']);
+        $user = User::factory()->create(["username" => 'any_user']);
+        $post = Post::factory()->create(["user_id" => $user->id]);
+        Auth::setUser($user);
+        $mock_data = [
+            "post_id" => $post->id,
+            "content" => $this->faker->realText(700) . " {User}mention_user{/User}",
+        ];
+        $response = $this->post('api/quotepost',$mock_data);
+        $response->assertStatus(201);
+        $this->assertDatabaseHas('reply_posts', [
+            "user_id" => $mention_user->id,
+            "post_id" => $post->id
+        ]);
+    }
 }
